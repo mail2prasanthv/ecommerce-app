d7079e845f8e1b4e5343ec4f8521f55b73205150
Prasanth Vaghese <mail2prasanthv@gmail.com>
2022-12-27 21:30:08 +0530

    Step 8: Add image links

--- src/App.js
+++ src/App.js
@@ -4,35 +4,44 @@ import "./categories.styles.scss";
 function App() {
 
   const categories = [
-     {
-      id   : 1,
-      title: 'HATS'
-     },
-     {
-      id   : 2,
-      title: 'JACKETS'
-     },
-     {
-      id   : 3,
-      title: 'SNEAKERS'
-     },
-     {
-      id   : 4,
-      title: 'WOMENS'
-     },
-     {
-      id   : 5,
-      title: 'MENS'
-     }
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
   return (
     <div className="categories-container">
       {
-      categories.map((eachCategory)=>
-                      <div className="category-container">
-                        <div className="category-image"/>
+      categories.map(({id, title, imageUrl})=>
+                      <div key={id} className="category-container">
+                        <div className="category-image" 
+                        style={{
+                          backgroundImage: `url(${imageUrl})`
+                        }}/>
+
                         <div className="category-body-container">
-                          <h2>{eachCategory.title}</h2>
+                          <h2>{title}</h2>
                           <h3>Shop Now</h3>
                         </div>
                       </div>

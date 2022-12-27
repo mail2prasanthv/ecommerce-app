494c8a8de8f4206e96a88e972a62f50781365c3e
Prasanth Vaghese <mail2prasanthv@gmail.com>
2022-12-27 18:20:33 +0530

    Skeleton

--- src/App.js
+++ src/App.js
@@ -2,8 +2,49 @@
 
 function App() {
   return (
-    <div>
-      Hello World.
+    <div className="categories-container">
+
+      <div className="category-container">
+        {/*image*/}
+        <div className="category-body-container">
+          <h2>HATS</h2>
+          <h3>Shop Now</h3>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*image*/}
+        <div className="category-body-container">
+          <h2>JACKETS</h2>
+          <h3>Shop Now</h3>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*image*/}
+        <div className="category-body-container">
+          <h2>SNEAKERS</h2>
+          <h3>Shop Now</h3>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*image*/}
+        <div className="category-body-container">
+          <h2>WOMENS</h2>
+          <h3>Shop Now</h3>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*image*/}
+        <div className="category-body-container">
+          <h2>MENS</h2>
+          <h3>Shop Now</h3>
+        </div>
+      </div>
+
+      
     </div>
   );
 }

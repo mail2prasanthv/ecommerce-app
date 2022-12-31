fcd34cc2fc3915c8c659fece7b656f4f63a1a2ae
Prasanth Vaghese <mail2prasanthv@gmail.com>
2022-12-31 14:53:24 +0530

    Step 11j. Add routes to various links.

--- src/components/navigation-pane/navigationpane.component.jsx
+++ src/components/navigation-pane/navigationpane.component.jsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import {ReactComponent as ApplicationLogo} from "../../assets/crown.svg"
+import "./navigation.styles.scss"
 
 const NavigationPane=() =>{
 
@@ -14,25 +15,26 @@ const NavigationPane=() =>{
         <div className="navigation">
 
         <Link className='logo-container' to="/">
-          <ApplicationLogo/>
+          <ApplicationLogo className="logo"/>
 
         </Link>
 
-          <div className="links-container">
+          <div className="nav-links-container ">
             <Link className='nav-link' to="/shop">Shop</Link> 
           </div>
 
-          <div className="links-container">
+          <div className="nav-links-container ">
             <Link className='nav-link' to="/contact">Contact</Link> 
           </div>
 
-          <div className="links-container">
+          <div className="nav-links-container ">
             <Link className='nav-link' to="/signin">Sign In</Link> 
           </div>
 
           
-          <Outlet/>
+          
         </div>
+        <Outlet/>
       </Fragment>
       );
 };

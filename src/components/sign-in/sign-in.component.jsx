61036098f29fc57a4b548f69ae750b13c7f2fb09
Prasanth Vaghese <mail2prasanthv@gmail.com>
2023-01-01 17:22:11 +0530

    Step 12: Google Authentication

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,15 +1,22 @@
 
 import Directory from "../directory/directory.component";
 import { Outlet } from "react-router-dom";
+import {signInWithGooglePopup} from '../../utils/firebase/firebase.utils'
 
 const SignIn=() =>{
 
+  const logGoogleuser= async() => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  }
 
    
       return (
         <div>
-       
           <h1>sign In Page</h1>
+          <button onClick={logGoogleuser}>
+            Sign in with Google
+          </button>
         </div>
         
       );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,1 +1,27 @@
-import {} from 'firebase/app'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDR2ZOjN54T4GRXDda-8gGJU8gznwwc6ZM",
+  authDomain: "ecommerce-app-e8630.firebaseapp.com",
+  projectId: "ecommerce-app-e8630",
+  storageBucket: "ecommerce-app-e8630.appspot.com",
+  messagingSenderId: "637218688942",
+  appId: "1:637218688942:web:c0109369ce9e813020023f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup=()=>signInWithPopup(auth,provider);
